fc4842676af78bc9712600b0b46fcdcea30983d6
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-10-28 16:32:14 -0300

    feat: implementa teste para relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "headphone",
+        "TecInova",
+        "2021-07-04",
+        "2029-02-09",
+        "FR48",
+        "em caixas"
+        )
+
+    assert (
+        str(product) == f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
