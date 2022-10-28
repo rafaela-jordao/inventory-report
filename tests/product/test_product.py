9a2f8238379f8090f77a22703e1d826a6b797792
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-10-28 16:02:36 -0300

    feat: implementa teste para construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,6 +1,21 @@
-# iniciando o projeto
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "headphone",
+        "TecInova",
+        "2021-07-04",
+        "2029-02-09",
+        "FR48",
+        "em caixas"
+        )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "headphone"
+    assert product.nome_da_empresa == "TecInova"
+    assert product.data_de_fabricacao == "2021-07-04"
+    assert product.data_de_validade == "2029-02-09"
+    assert product.numero_de_serie == "FR48"
+    assert product.instrucoes_de_armazenamento == "em caixas"
